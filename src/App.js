a9886595b8b7d150d3bef03837b62c505a48281a
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-06-20 21:45:02 +0500

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Routes from './Routes';
 function App() {
   return (
     <>
-       {/* <Routes/> */}
-
-       <h1 className="text-3xl font-bold underline text-red-300 ">
-      Hello world!
-    </h1>
+       <Routes/>
     </>
   );
 }
